c0ef2cda66785122d6154dc5cf0860a83f33b3a4
Havrylenko Olexii <hristinson@gmail.com>
2025-12-02 18:18:21 +0200

    add:version

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Головна</h1>
+    <h1>Головна {{ version }}</h1>
     <NuxtLink to="/about">Перейти на About</NuxtLink><br />
     <NuxtLink to="/addProjectForm">Додати проект</NuxtLink><br />
     <NuxtLink to="/addTaskForm">Додати таску до проекта</NuxtLink><br />
@@ -8,3 +8,10 @@
     <NuxtLink to="/projectTasksList">Список</NuxtLink><br />
   </div>
 </template>
+
+<script setup lang="ts">
+// @ts-ignore
+import pkg from "../package.json";
+
+const version = pkg.version;
+</script>
